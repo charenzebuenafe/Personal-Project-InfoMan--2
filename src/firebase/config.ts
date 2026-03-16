3cd976c5f74ce176e7e721f66efe4c7680bebef8
Grah Boom <deepwokencuh@gmail.com>
2026-03-16 02:54:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3190301766-ede6d",
+  "appId": "1:675165692844:web:edffd07b59c3ec4f469009",
+  "apiKey": "AIzaSyDGI1wZu5G-r75aSoH3SUx87l_Jxqrs4Ao",
+  "authDomain": "studio-3190301766-ede6d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "675165692844"
+};
